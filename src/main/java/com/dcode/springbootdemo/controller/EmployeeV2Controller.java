96b5ad02c969594fa736c6df23270409bd934112
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-11-21 23:01:24 +0530

    get employees and delete route

--- src/main/java/com/dcode/springbootdemo/controller/EmployeeV2Controller.java
+++ src/main/java/com/dcode/springbootdemo/controller/EmployeeV2Controller.java
@@ -4,10 +4,9 @@ import com.dcode.springbootdemo.model.Employee;
 import com.dcode.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/employees")
@@ -20,5 +19,20 @@ public class EmployeeV2Controller {
     @PostMapping
     public Employee save(@RequestBody Employee employee) {
         return employeeService.save(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable String id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployeeById(@PathVariable String id) {
+        employeeService.deleteEmployeeById(id);
     }
 }
--- src/main/java/com/dcode/springbootdemo/service/EmployeeV2ServiceImpl.java
+++ src/main/java/com/dcode/springbootdemo/service/EmployeeV2ServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeV2ServiceImpl implements EmployeeService {
@@ -31,16 +32,24 @@ public class EmployeeV2ServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<EmployeeEntity> employeeEntityList = employeeRepository.findAll();
+        return employeeEntityList.stream().map(employeeEntity -> {
+            Employee employee = new Employee();
+            BeanUtils.copyProperties(employeeEntity, employee);
+            return employee;
+        }).collect(Collectors.toList());
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
     }
 
     @Override
     public void deleteEmployeeById(String id) {
-
+        employeeRepository.deleteById(id);
     }
 }
